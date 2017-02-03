e57fe7845f158fc86580fdafae6960915738d0dc
philippe.roy <philippe.roy@carsales.com.au>
2017-02-03 15:20:13 +1100

    [MOTO-1432] - Editorial Desktop > Review Ratings

--- Src/Csn.Retail.Editorial.Web/Features/Details/Assets/Js/editorsRating-component.js
+++ Src/Csn.Retail.Editorial.Web/Features/Details/Assets/Js/editorsRating-component.js
@@ -3,6 +3,8 @@
 require('../css/Modules/Widgets/_editorsRatings.scss');
 
 import Modernizr from 'modernizr';
+import Circles from 'circles';
+import TinyAnimate from 'TinyAnimate'
 
 if (!Modernizr.meter) {
     // Meter polyfill
@@ -13,5 +15,47 @@ if (!Modernizr.meter) {
 }
 
 
+
+var myCircle = Circles.create({
+    id:                  'overall-rating-graph',
+    radius:              60,
+    value:               document.getElementById('overall-rating-graph').getAttribute('data-overall-rating'),
+    maxValue:            100,
+    width:               10,
+    text:                function(value){return value;},
+    colors:              ['#DBDBDB', '#007CC2'],
+    duration:            500,
+    wrpClass:            'overall-rating-graph__wrp',
+    textClass:           'overall-rating-graph__text',
+    valueStrokeClass:    'overall-rating-graph__valueStroke',
+    maxValueStrokeClass: 'overall-rating-graph__maxValueStroke',
+    styleWrapper:        true,
+    styleText:           true
+});
+
+
+let AnimateMeters = function(listOfMeter, timeBetween = 400) {
+
+    const LIST = listOfMeter;
+    const LENGTH = LIST.length;
+    const DELAY = timeBetween;
+    const w = window;
+
+    for (let i = 0; i < LENGTH; i++) {
+        w.setTimeout(AnimateMeter.bind(undefined,i,LIST), DELAY*i)
+    }
+
+}
+
+let AnimateMeter = function(index, list, duration = 1000) {
+
+    const VALUE = parseInt(list[index].getAttribute('data-value'));
+
+    TinyAnimate.animate(0, VALUE, duration, function(x) {
+        list[index].setAttribute('value', x);
+    }, 'easeOutCubic');
+}
+
+AnimateMeters(document.querySelectorAll('.expert-ratings__meter'), 200);
 
 
