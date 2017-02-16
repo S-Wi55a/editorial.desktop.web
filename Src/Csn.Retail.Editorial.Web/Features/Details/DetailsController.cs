076caed2559711832c1b217ba9ad0e97465a17a3
james.christou <james.christou@carsales.com.au>
2017-02-16 11:57:54 +1100

    Removed extra implementation of sitenav and using event handler based implementation instead

--- Src/Csn.Retail.Editorial.Web/Features/Details/DetailsController.cs
+++ Src/Csn.Retail.Editorial.Web/Features/Details/DetailsController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
 using System.Web.Mvc;
 using Csn.Retail.Editorial.Web.Features.Details.Models;
+using Csn.Retail.Editorial.Web.Features.Home;
+using Csn.Retail.Editorial.Web.Features.Shared.GlobalSite;
 using Csn.Retail.Editorial.Web.Features.Shared.Proxies.EditorialApi;
 using Csn.SimpleCqrs;
 
@@ -9,21 +11,28 @@ namespace Csn.Retail.Editorial.Web.Features.Details
     public class DetailsController : Controller
     {
         private readonly IQueryDispatcher _queryDispatcher;
+        private readonly IEventDispatcher _eventDispatcher;
 
-        public DetailsController(IQueryDispatcher queryDispatcher)
+        public DetailsController(IQueryDispatcher queryDispatcher, IEventDispatcher eventDispatcher)
         {
             _queryDispatcher = queryDispatcher;
+            _eventDispatcher = eventDispatcher;
         }
 
         [Route("editorial/details/{pageName:regex(^.*-\\d+/?$)}")]
         // GET: Details
         public async Task<ActionResult> Index(ArticleIdentifier articleIdentifier)
         {
-            var viewModel =
-                await _queryDispatcher.DispatchAsync<GetArticleQuery, ArticleViewModel>(new GetArticleQuery()
+            var dispatchedEvent = _eventDispatcher.DispatchAsync(new DetailsPageRequestEvent());
+            
+            var dispatchedQuery = _queryDispatcher.DispatchAsync<GetArticleQuery, ArticleViewModel>(new GetArticleQuery()
                 {
                     Id = articleIdentifier.Id
                 });
+
+            await Task.WhenAll(dispatchedEvent, dispatchedQuery);
+
+            var viewModel = dispatchedQuery.Result;
 
             // TODO: add error handling
             //if (viewModel == null)
@@ -34,6 +43,10 @@ namespace Csn.Retail.Editorial.Web.Features.Details
             return View("DefaultTemplate", viewModel);
         }
 
+    }
+
+    public class DetailsPageRequestEvent : IEvent, IRequireGlobalSiteNav
+    {
     }
 }
 
--- Src/Csn.Retail.Editorial.Web/Features/SiteNav/SiteNavQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using Csn.SimpleCqrs;
-
-namespace Csn.Retail.Editorial.Web.Features.SiteNav
-{
-    public class SiteNavQuery : IQuery
-    {
-        public bool RefreshCache { get; set; }
-    }
-}
