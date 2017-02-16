ddd74710a44183c02901133f7cfdabefc7c6e522
jony.wijaya <jony.wijaya@carsales.com.au>
2017-02-17 09:22:19 +1100

    Added MoreArticleData

---
+++ Src/Csn.Retail.Editorial.Web/Features/Shared/Models/MoreArticleItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Csn.Retail.Editorial.Web.Features.Shared.Models
+{
+    public class MoreArticleItem
+    {
+        public string Filter { get; set; }
+        public string Title { get; set; }
+        public string Uri { get; set; }
+    }
+}
