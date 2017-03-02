4395b257ee888f5df7474e0b92ccb8c0ba43babf
philippe.roy <philippe.roy@carsales.com.au>
2017-03-03 09:40:38 +1100

    Remove References to Lory and slideshow

--- Src/Csn.Retail.Editorial.Web/Features/Details/Assets/Js/MoreArticles/moreArticlesContentView.js
+++ Src/Csn.Retail.Editorial.Web/Features/Details/Assets/Js/MoreArticles/moreArticlesContentView.js
@@ -1,7 +1,7 @@
 ﻿export default (data) => {
     const template = data.Items.map((item) => {
         return (`
-            <li class="more-article lory-slider__slide">
+            <div class="more-article more-articles__slide swiper-slide">
                     <a class="more-article__link-container" href="${item.Url}">
                         <div class="more-article__image">
                             <img src="${item.Image.Url}?width=140&height=93" alt="${item.Image.AlternateText}" />
@@ -16,7 +16,7 @@
                             : ''}
                         </div>
                     </a>
-                </li>
+                </div>
             `)
     })
 
