ef36b9974c4aced47906bc27ac9d6b97ef9ab3f4
philippe.roy <philippe.roy@carsales.com.au>
2017-03-02 16:11:45 +1100

    Updated Slider

--- Src/Csn.Retail.Editorial.Web/Features/Details/Assets/details-page.js
+++ Src/Csn.Retail.Editorial.Web/Features/Details/Assets/details-page.js
@@ -8,7 +8,6 @@ import { inViewport } from 'Js/Modules/InViewport/inViewport.js';
 
 //------------------------------------------------------------------------------------------------------------------
 
-
 // Dynamically set the public path for ajax/code-split requests
 let scripts = document.getElementsByTagName("script");
 let scriptsLength = scripts.length;
@@ -23,59 +22,111 @@ for (var i = 0; i < scriptsLength; i++) {
 
 //------------------------------------------------------------------------------------------------------------------
 
-require.ensure(['swiper', 'Js/Modules/Modal/modal.js'],
+// APP
+let aboveTheFold = function() {
+    const multipleImageLayout = document.querySelector('.hero--multipleImages')
+    const imageAndVideoLayout = document.querySelector('.hero--imageAndVideo')
+
+    if (multipleImageLayout || imageAndVideoLayout) {
+        require.ensure(['swiper', 'Js/Modules/Modal/modal.js'],
     function(require) {
         Swiper = require('swiper')
 
-        const heroSwiper = new Swiper('.hero .slideshow__container', {
-            // Optional parameters
-            loop: true,
-            slidesPerView: 3,
-            slidesPerGroup: 1,
+        if (multipleImageLayout) {
+            const heroSwiper = new Swiper('.hero .slideshow__container', {
+                // Optional parameters
+                loop: true,
+                slidesPerView: 3,
+                slidesPerGroup: 1,
 
-            //Progress
-            watchSlidesProgress: true,
-            watchSlidesVisibility: true,
+                //Progress
+                watchSlidesProgress: true,
+                watchSlidesVisibility: true,
 
-            //Images
-            preloadImages: false,
-            lazyLoading: true,
-            lazyLoadingInPrevNext: true,
-            lazyLoadingInPrevNextAmount: 3, // Can't be less than slidesPerView
+                //Slides grid
+                centeredSlides: true,
 
-            // Navigation arrows
-            nextButton: '.slideshow__nav--next',
-            prevButton: '.slideshow__nav--prev',
+                //Images
+                preloadImages: false,
+                lazyLoading: true,
+                lazyLoadingInPrevNext: true,
+                lazyLoadingInPrevNextAmount: 3, // Can't be less than slidesPerView
 
-            //Namespace
-            containerModifierClass: 'slideshow',
-            wrapperClass: 'slideshow__slides',
-            slideClass: 'slideshow__slide',
-            lazyLoadingClass: 'slideshow__image',
-            lazyStatusLoadingClass: 'slideshow__image--loading',
-            lazyStatusLoadedClass: 'slideshow__image--loaded',
-            lazyPreloaderClass: 'slideshow__image--preloader',
+                // Navigation arrows
+                nextButton: '.slideshow__nav--next',
+                prevButton: '.slideshow__nav--prev',
 
-            //Breakpoints
-            breakpoints: {
-                1199: {
-                    slidesPerView: 2,
+                //Namespace
+                containerModifierClass: 'slideshow',
+                wrapperClass: 'slideshow__slides',
+                slideClass: 'slideshow__slide',
+                lazyLoadingClass: 'slideshow__image',
+                lazyStatusLoadingClass: 'slideshow__image--loading',
+                lazyStatusLoadedClass: 'slideshow__image--loaded',
+                lazyPreloaderClass: 'slideshow__image--preloader',
+
+                //Breakpoints
+                breakpoints: {
+                    1199: {
+                        slidesPerView: 2,
+                    }
                 }
-            }
-        })
+            })
+        } else if (imageAndVideoLayout) {
+            const heroSwiper = new Swiper('.hero .slideshow__container', {
+                // Optional parameters
+                loop: true,
+                slidesPerView: 2,
+                slidesPerGroup: 1,
+
+                //Progress
+                watchSlidesProgress: true,
+                watchSlidesVisibility: true,
+
+                //Images
+                preloadImages: false,
+                lazyLoading: true,
+                lazyLoadingInPrevNext: true,
+                lazyLoadingInPrevNextAmount: 2, // Can't be less than slidesPerView
+
+                // Navigation arrows
+                nextButton: '.slideshow__nav--next',
+                prevButton: '.slideshow__nav--prev',
+
+                //Namespace
+                containerModifierClass: 'slideshow',
+                wrapperClass: 'slideshow__slides',
+                slideClass: 'slideshow__slide',
+                lazyLoadingClass: 'slideshow__image',
+                lazyStatusLoadingClass: 'slideshow__image--loading',
+                lazyStatusLoadedClass: 'slideshow__image--loaded',
+                lazyPreloaderClass: 'slideshow__image--preloader',
+
+                //Breakpoints
+                breakpoints: {
+                    1199: {
+                        slidesPerView: 1,
+                    }
+                }
+            })
+        }
+
 
         if (document.querySelector('[data-ajax-modal]')) {
 
             //init Modal JS
             require('Js/Modules/Modal/modal.js');
 
-            var modalSlideshow = null;
-
             // load the modal slider after the ajax-event has completed //TODO: not using ajax anymore find better name
             window.addEventListener('ajax-completed',
                 function() {
 
+                    const initSlide = !!document.querySelector('.slideshow--modal') ? parseInt(document.querySelector('.slideshow--modal').getAttribute('data-slideshow-start')) : 0
+                    console.log(initSlide)
                     const modalSwiper = new Swiper('._c-modal .slideshow', {
+
+                        initialSlide: initSlide,
+
                         // Optional parameters
                         loop: true,
                         slidesPerView: 1,
@@ -106,105 +157,15 @@ require.ensure(['swiper', 'Js/Modules/Modal/modal.js'],
 
                     })
 
-                    window.modalSwiper = modalSwiper;
-
-
-            // Resize Handler for modal window to shrink when window height is less than image
-            let addSizeHandlers = function() {
-
-                let modalContent = document.querySelector('._c-modal__content');
-                let el = modalContent.querySelector('._c-modal .slideshow');
-                let threshold = 80; // TODO: find way to dynamically set spacing
-                let windowHeight = window.innerHeight - threshold;
-
-                el.style.width = '';
-                modalSwiper.onResize()
-
-                // Compare image dimensions with window
-                if (windowHeight < el.offsetHeight) {
-
-                    let imageRatio = 3 / 2;
-                    let width = Math.round((imageRatio * windowHeight));
-
-                    el.style.width = (Math.round(width)) + 'px';
-
-                    //then force slider re position
-                    modalSwiper.onResize()
-                }
-            }
-
-            // handle event
-            window.addEventListener('resize', addSizeHandlers)
-
-            window.addEventListener('modal.close', function() {
-                window.removeEventListener('resize', addSizeHandlers)
-
-            })
-
-           });
-        }
-    },
-    'Hero Slidier');
-
-
-// APP
-let aboveTheFold = function() {
-    // If Slideshow then must have Modal
-    if (document.querySelector('[data-slideshow]')) {
-
-        // Correct the size when the brightcove video is smaller than
-        if (document.querySelector('.hero .brightcove__iframe-wrapper') && document.querySelector('._c-slideshow__slides')) {
-            window.addEventListener('after.csn-slider.lazyload', function() {
-                document.querySelector('.hero .brightcove__iframe-wrapper').style.paddingTop = document.querySelector('._c-slideshow__slides').offsetHeight + "px"
-                window.addEventListener('resize', () => {
-                    document.querySelector('.hero .brightcove__iframe-wrapper').style.paddingTop = document.querySelector('._c-slideshow__slides').offsetHeight + "px"
-                })
-            })
-        }
-
-        //Lazy load - Above the fold because some detail layouts don't need it
-        require.ensure([
-                "Js/Modules/Slideshow/slideshow.js", "Js/Modules/Modal/modal.js"
-        ],
-            function() {
-
-                var Slideshow = require("Js/Modules/Slideshow/slideshow.js").default;
-
-                //Setup slideshow
-                Slideshow({
-                    scope: '[data-slideshow]',
-                    showPages: false,
-                    lazyLoad: true,
-                    infinity: true
-                });
-
-                if (document.querySelector("[data-ajax-modal]")) {
-
-                    //init Modal JS
-                    require("Js/Modules/Modal/modal.js");
-
-                    var modalSlideshow = null;
-
-                    // load the modal slider after the ajax-event has completed //TODO: not using ajax anymore find better name
-                    window.addEventListener('ajax-completed',
-                        function() {
-                            //Init modal slideshow
-                            modalSlideshow = Slideshow({
-                                scope: '[data-slideshow-modal]', //this needs to be unique
-                                pageBy: 1,
-                                showPages: false,
-                                lazyLoad: true,
-                                infinity: true
-                            })
-                        });
-
                     // Resize Handler for modal window to shrink when window height is less than image
-                    let addSizeHandlers = function() {
+                    let resizeHandlers = function(windowHeightThreshold) {
 
                         let modalContent = document.querySelector('._c-modal__content');
-                        let el = modalContent.querySelector('._c-slideshow__slides');
-                        let threshold = 80; // TODO: find way to dynamically set spacing
-                        let windowHeight = window.innerHeight - threshold;
+                        let el = modalContent.querySelector('._c-modal .slideshow');
+                        let windowHeight = window.innerHeight - windowHeightThreshold;
+
+                        el.style.width = '';
+                        modalSwiper.onResize()
 
                         // Compare image dimensions with window
                         if (windowHeight < el.offsetHeight) {
@@ -215,24 +176,30 @@ let aboveTheFold = function() {
                             el.style.width = (Math.round(width)) + 'px';
 
                             //then force slider re position
-                            el.style.transform = 'translate3d(-' + width * modalSlideshow.currentSlide() + 'px,0,0)';
-                            el.style.webkitTransform = 'translate3d(-' + width * modalSlideshow.currentSlide() + 'px,0,0)';
+                            modalSwiper.onResize()
                         }
                     }
 
-                    let modalInner = document.querySelector('._c-modal__inner');
+                    let modalResizeHandler = resizeHandlers.bind(null, 80);
 
-                    ['after.csn-slider.lazyload', 'after.csn-slider.resize'].forEach(function(e) {
-                        modalInner.addEventListener(e, addSizeHandlers);
-                    });
+                    // Call it once to get approiate size on first view
+                    modalResizeHandler()
 
-                }
+                    // handle event
+                    window.addEventListener('resize', modalResizeHandler)
 
-            },
-            'details-page--slideshow-and-modal');
+                    window.addEventListener('modal.close', function() {
+                        window.removeEventListener('resize', modalResizeHandler)
+
+                    })
+
+                });
+        }
+    },
+    'Hero Slidier');
     }
 }
-//aboveTheFold();
+aboveTheFold();
 
 //Editors Rating
 let editorRatings = function() {
