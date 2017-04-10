abf73200953471e65a7515df63db00d6aea4d06f
james.christou <james.christou@carsales.com.au>
2017-04-10 10:15:09 +1000

    Removing unused references

--- Src/Csn.Retail.Editorial.Web/Features/Details/Mappings/UseDropCaseResolver.cs
+++ Src/Csn.Retail.Editorial.Web/Features/Details/Mappings/UseDropCaseResolver.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using AutoMapper;
 using Csn.Retail.Editorial.Web.Features.Details.Models;
 using Csn.Retail.Editorial.Web.Features.Shared.Proxies.EditorialApi;
