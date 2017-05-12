19e0760cb99447ac8997573444bf3be733f9aac5
james.christou <james.christou@carsales.com.au>
2017-05-13 07:44:05 +1000

    [MOTO-1572]: Adding redirect strategies for cars, bikes and boats taken from csn.editorial

---
+++ Src/Csn.Retail.Editorial.Web/Infrastructure/Filters/RedirectAttributeFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Csn.Retail.Editorial.Web.Infrastructure.Filters
+{
+    public class RedirectAttributeFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.IsChildAction) return;
+
+            var strategies =
+                (DependencyResolver.Current.GetServices<IRedirectStrategy>() ?? Enumerable.Empty<IRedirectStrategy>())
+                .OrderBy(x => x.Order).ToList();
+
+            foreach (var redirectStrategy in strategies)
+            {
+                var instruction = redirectStrategy.Apply(filterContext);
+
+                if (!instruction.IsRedirectRequired) continue;
+
+                filterContext.Result = new RedirectResult(instruction.Url, instruction.IsPermanent);
+
+                break;
+            }
+        }
+    }
+}
---
+++ Src/Csn.Retail.Editorial.Web/Infrastructure/Redirects/HttpRequestContextWrapper.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Web;
+using Bolt.Common.Extensions;
+
+namespace Csn.Retail.Editorial.Web.Infrastructure.Redirects
+{
+    public interface IRequestContextWrapper
+    {
+        bool IsSecure();
+
+        NameValueCollection Headers { get; }
+
+        NameValueCollection QueryString { get; }
+
+        Uri Url { get; }
+
+        bool IsMobile();
+
+        bool IsIpad();
+
+        bool IsTouchDevice();
+    }
+
+    public class HttpRequestContextWrapper : IRequestContextWrapper
+    {
+        public bool IsSecure()
+        {
+            return HttpContext.Current.NullSafeGet(context =>
+            {
+                var isSecure = context.Request.IsSecureConnection;
+
+                return isSecure || context.Request.Headers["IsSecure"].ToBoolean().NullSafe();
+            });
+        }
+
+        public NameValueCollection Headers
+        {
+            get { return HttpContext.Current.NullSafeGet(x => x.Request.Headers); }
+        }
+
+        public NameValueCollection QueryString
+        {
+            get { return HttpContext.Current.NullSafeGet(x => x.Request.QueryString); }
+        }
+
+        public Uri Url { get { return HttpContext.Current.NullSafeGet(x => x.Request.Url); } }
+
+        public bool IsMobile()
+        {
+            if (HttpContext.Current == null || HttpContext.Current.Request == null) return false;
+
+            var request = HttpContext.Current.Request;
+
+            bool isMobile = (request.Browser != null && request.Browser.IsMobileDevice);
+
+            string userAgent = (!string.IsNullOrEmpty(request.UserAgent)) ? request.UserAgent.ToLower() : string.Empty;
+
+            // Check for mobile devices, excluding iPad.
+            if ((isMobile
+                || userAgent.Contains("iphone")
+                || userAgent.Contains("ipod")
+                || userAgent.Contains("blackberry")
+                || userAgent.Contains("mobile")
+                || userAgent.Contains("windows ce")
+                || userAgent.Contains("opera mini")
+                || userAgent.Contains("palm"))
+                && !userAgent.Contains("ipad"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsIpad()
+        {
+            return HttpContext.Current.NullSafeGet(cxt => cxt
+                        .Request
+                        .UserAgent
+                        .NullSafe()
+                        .IndexOf("ipad", StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        public bool IsTouchDevice()
+        {
+            if (HttpContext.Current == null || HttpContext.Current.Request == null) return false;
+
+            var request = HttpContext.Current.Request;
+
+            bool isMobile = (request.Browser != null && request.Browser.IsMobileDevice);
+
+            string userAgent = (!string.IsNullOrEmpty(request.UserAgent)) ? request.UserAgent.ToLower() : string.Empty;
+
+            if (isMobile
+               || userAgent.Contains("iphone")
+               || userAgent.Contains("ipod")
+               || userAgent.Contains("blackberry")
+               || userAgent.Contains("mobile")
+               || userAgent.Contains("windows ce")
+               || userAgent.Contains("opera mini")
+               || userAgent.Contains("palm")
+               || userAgent.Contains("ipad")
+               || userAgent.Contains("touch"))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
