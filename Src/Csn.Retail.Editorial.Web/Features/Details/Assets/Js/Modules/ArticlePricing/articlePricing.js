be93b0150749e6eb2b07570f56f01f25e2f884b4
james.christou <james.christou@carsales.com.au>
2017-06-29 15:40:20 +1000

    Fixing bug with article pricing

--- Src/Csn.Retail.Editorial.Web/Features/Details/Assets/Js/Modules/ArticlePricing/articlePricing.js
+++ Src/Csn.Retail.Editorial.Web/Features/Details/Assets/Js/Modules/ArticlePricing/articlePricing.js
@@ -4,7 +4,11 @@ import {disclaimerTemplate} from 'Js/Modules/Modal/modal-disclaimer-view'
 // display disclaimer on pricing guide
 window.csn_modal = window.csn_modal || new Modal();
 
-document.querySelector('.article__pricing-label').addEventListener('click',
-    (e) => {
-        window.csn_modal.show(disclaimerTemplate(e.target.getAttribute('data-disclaimer'), 'pricing-guide-disclaimer'));
-    });
+let pricingLabel = document.querySelector('.article__pricing-label');
+
+if (pricingLabel) {
+    test.addEventListener('click',
+        (e) => {
+            window.csn_modal.show(disclaimerTemplate(e.target.getAttribute('data-disclaimer'), 'pricing-guide-disclaimer'));
+        });
+}
