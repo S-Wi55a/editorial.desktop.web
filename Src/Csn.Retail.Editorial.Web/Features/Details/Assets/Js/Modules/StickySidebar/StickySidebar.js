61aa3c85f04e60cd8ff0ce1d434d1a096ad88a71
philippe.roy <philippe.roy@carsales.com.au>
2017-08-22 14:01:31 +1000

    Add scenerio where side bar can me smaller than window height

--- Src/Csn.Retail.Editorial.Web/Features/Details/Assets/Js/Modules/StickySidebar/StickySidebar.js
+++ Src/Csn.Retail.Editorial.Web/Features/Details/Assets/Js/Modules/StickySidebar/StickySidebar.js
@@ -1,7 +1,7 @@
 ﻿import ScrollMagic from 'ScrollMagic'
 import ResizeSensor from 'css-element-queries/src/ResizeSensor'
 import * as Utils from 'Features/Details/Assets/Js/Modules/StickySidebar/stickySidebar.utils.js'
-import { scrollingUp, scrollingDown } from 'Features/Details/Assets/Js/Modules/StickySidebar/stickySidebar.Actions.js'
+import { scrollingUp, scrollingDown, scrollingSimple } from 'Features/Details/Assets/Js/Modules/StickySidebar/stickySidebar.Actions.js'
 
 if (process.env.DEBUG) { require('debug.addIndicators'); }
 
@@ -25,38 +25,66 @@ export function init(d, w, aside) {
          
     // Scroll Magic Controller
     w.scrollMogicController = w.scrollMogicController || new ScrollMagic.Controller();
+
+    let sceneSimple = new ScrollMagic.Scene({
+            triggerElement: aside,
+            triggerHook: 1 - (w.innerHeight - references.siteNavHeight)/w.innerHeight,
+        })  
+        .addTo(w.scrollMogicController);
   
     let sceneDown = new ScrollMagic.Scene({
             triggerElement: aside,
             triggerHook: Utils.elementScreenRealEstate('.more-articles').heightInPercentage,
             offset: aside.offsetHeight 
         })
-        .on('update', (e)=>{scrollingDown(pin, aside, references, e)}) 
         .addTo(w.scrollMogicController);
 
     let sceneUp = new ScrollMagic.Scene({
             triggerElement: aside,
             triggerHook: 1 - (w.innerHeight - references.siteNavHeight)/w.innerHeight,
-        })
-        .on('update', (e)=>{scrollingUp(pin, aside, references, e)}) 
+        })       
         .addTo(w.scrollMogicController);
+
+    const boundScrollingSimple = (e)=>{scrollingSimple(pin, aside, references, e)}
+    const boundScrollingDown = (e)=>{scrollingDown(pin, aside, references, e)}
+    const boundScrollingUp = (e)=>{scrollingUp(pin, aside, references, e)}
+
+    //Check ScreenSize
+    function screenSizeCheck() {
+        if (aside.offsetHeight < window.innerHeight) {
+            sceneDown.off('update', boundScrollingDown) 
+            sceneUp.off('update', boundScrollingUp) 
+            sceneSimple.on('update', boundScrollingSimple)
+        } 
+        else {
+            sceneSimple.off('update', boundScrollingSimple)
+            sceneDown.on('update', boundScrollingDown) 
+            sceneUp.on('update', boundScrollingUp) 
+        }
+    }
+
+    screenSizeCheck();
 
     //As modules load in the sidebar the hight grows, ResizeSensor has a callback when the size changes
     new ResizeSensor(aside, function() {
         sceneDown.offset(aside.offsetHeight)
+        screenSizeCheck();
     });
 
     // handle event
-    window.addEventListener("optimizedResize", function() {
-    });
+    window.addEventListener("optimizedResize", screenSizeCheck);
 
     // For Debugging
     if (process.env.DEBUG) {
         w.sceneDown = sceneDown
         w.sceneUp = sceneUp
+        w.sceneSimple = sceneSimple
+
 
         sceneDown.addIndicators({ name: "sceneDown" })   
         sceneUp.addIndicators({ name: "sceneUp" })   
+        sceneSimple.addIndicators({ name: "sceneSimple" })   
+
 
     }    
 }
--- Src/Csn.Retail.Editorial.Web/Features/Details/Assets/Js/Modules/StickySidebar/stickySidebar.Actions.js
+++ Src/Csn.Retail.Editorial.Web/Features/Details/Assets/Js/Modules/StickySidebar/stickySidebar.Actions.js
@@ -114,7 +114,90 @@ export function scrollingDown(pin, el, ref, e) {
 
 }
 
-   
+export function scrollingSimple(pin, el, ref, e) {
+
+    const state = {
+        STATE: e.target.state(),
+        FORWARD: e.target.controller().info('scrollDirection') === 'FORWARD',
+        REVERSE: e.target.controller().info('scrollDirection') === 'REVERSE',
+        REACHED_TOP: e.scrollPos <= ref.startingCoordinatesTop(),
+        REACHED_BOTTOM: (e.scrollPos + el.getBoundingClientRect().bottom) >= ref.footerCoordinatesTop()
+    }
+
+    if (state.FORWARD) {
+        lock.up = false; // the lock is for debouncing
+
+        //Reached footer
+        if (state.REACHED_BOTTOM) {
+            if (process.env.DEBUG) { console.log('simple Down 1') }
+               
+            const css = {  
+                top: ref.footerCoordinatesTop() - el.offsetHeight  + 'px',
+                position: 'absolute'
+            }
+            pin.state = false
+            Utils.setStylesForElement(el, css)
+
+        }
+        else if (state.STATE === 'DURING' && !state.REACHED_BOTTOM) {
+            if (process.env.DEBUG) { console.log('Simple Down 2') }
+
+            const css = {
+                top: ref.siteNavHeight + 'px',
+                position: 'fixed'
+            }
+            pin.state = true
+            lock.up = true
+            Utils.setStylesForElement(el, css)
+        
+        }
+        
+    }
+
+    if (state.REVERSE) {
+
+        if (process.env.DEBUG) { console.log("Scence Up Simple :: pinState: " + pin.state + " State: " + state.STATE + " Reached Top: " + state.REACHED_TOP + " Lock Up: " + lock.up)}
+
+        //Reached Top
+        if (state.REACHED_TOP) {
+            if (process.env.DEBUG) { console.log('Simple Up 1') }
+
+            const css = {
+                top: 'auto',
+                position: 'static',
+            }
+            pin.state = false
+            lock.up = false
+            Utils.setStylesForElement(el, css)
+        }
+        // The top trigger has passed the trigger hook
+        else if (pin.state && state.STATE === 'DURING' && !lock.up) {
+            if (process.env.DEBUG) { console.log('Simple Up 2') }
+
+            const css = {  
+                top: Utils.distance(ref.wrapper.getBoundingClientRect().top, el.getBoundingClientRect().top, ref.startingCoordinatesTop()) + 'px',
+                position: 'absolute',
+            }
+            pin.state = false
+            Utils.setStylesForElement(el, css)
+
+        }
+        // The top trigger has not passed the trigger hook
+        else if (state.STATE === 'BEFORE' && !state.REACHED_TOP && !lock.up) {
+            if (process.env.DEBUG) { console.log('Simple Up 3') }
+
+            const css = {
+                top: ref.siteNavHeight + 'px',
+                position: 'fixed'
+            }
+            pin.state = true
+            lock.up = true
+            Utils.setStylesForElement(el, css)
+        
+        }
+    } 
+
+}
             
   
 
