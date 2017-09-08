0e58d657588f33cabda62934664dded092e8d891
philippe.roy <philippe.roy@carsales.com.au>
2017-09-08 18:08:00 +1000

    refactor

--- Src/Csn.Retail.Editorial.Web/Features/Listings/Assets/Js/Modules/iNav/Components/iNavMenuHeaderItem.tsx
+++ Src/Csn.Retail.Editorial.Web/Features/Listings/Assets/Js/Modules/iNav/Components/iNavMenuHeaderItem.tsx
@@ -9,118 +9,57 @@ interface IINavMenuHeaderItemComponent {
   ui: any,
   node: any //TODO: update
   toggleIsSelected: (id: number) => any
-  index: number
+  index: number,
+  isActive: boolean  
 }
 
 const INavMenuHeaderItemComponent: React.StatelessComponent<IINavMenuHeaderItemComponent> = ({ isActive, node, toggleIsSelected, index }) => {
 
   return (
     <div className={['iNav__menu-header-item', isActive && 'isActive'].join(' ')} onClick={() => toggleIsSelected(index)}>
-      {node.displayName}{index}
+      {node.displayName}
     </div>
   )
 }
 
-
-// const INavMenuHeaderItemComponentConnect = connect(
-//   null,
-//   null
-// )(INavMenuHeaderItemComponent)
-
-
-// // Add the UI to the store
-// const INavMenuHeaderItem = ui({
-//   key: 'iNavNodesContainer',
-//   mapStateToProps: (componentState: any, ownProps: any) => {
-//     //debugger
-//     return {
-//       isActive: componentState.isVisibleId === ownProps.index
-//     }
-//   },
-//   mapDispatchToProps: (dispatch: Dispatch<any>) => {
-//     // ALL actions dispatched via 'dispatch' function above have the component key tagged to the action.
-//     // You can see that by inspecting action.meta.reduxFractalTriggerComponent in redux dev tools.
-//     // All local actions are dispatched also on the global store
-//     // You must return an object containing the keys that will become props to the component just like in react redux 'connect'
-//     return {
-//       toggleIsSelected: (id: number) => {
-//         dispatch({
-//           type: ActionTypes.TOGGLE_IS_ACTIVE,
-//           payload: {
-//             id
-//           }
-//         })
-//       }
-//     }
-//   },
-
-// })(INavMenuHeaderItemComponentConnect);
-
-
-
-// Redux Connect
-const mapStateToProps = (state: any) => {
-  return {
-      ui: state.local['iNavNodesContainer']
+const componentRootReducer = (state: any, action: any): any => {
+  // state represents *only* the UI state for this component's scope - not any children
+  switch (action.type) {
+    case ActionTypes.TOGGLE_IS_ACTIVE:
+      return {
+        ...state,
+        isActive: !state.isActive && state.id === action.payload.id
+      }
+    default:
+      return state
   }
 }
 
 const wrapper = compose(
-  
-  connect(
-      mapStateToProps
-  ),
-
   ui({
-      key: props => `iNavNodesContainerItem_${props.index}`,
-      createStore: (props: any) => {
-          return createStore(rootReducer, { id: props.index, isActive: false }) // NOTE if list dynamically changes, id may be incorrect unless handles by an action
-      },
-      mapStateToProps: (componentState: any, ownProps: any) => {
-          
-          return {
-              isActive: ownProps.index === ownProps.ui.isVisibleId
-          }
-      },
-      mapDispatchToProps: (dispatch: Dispatch<any>) => {
-        // ALL actions dispatched via 'dispatch' function above have the component key tagged to the action.
-        // You can see that by inspecting action.meta.reduxFractalTriggerComponent in redux dev tools.
-        // All local actions are dispatched also on the global store
-        // You must return an object containing the keys that will become props to the component just like in react redux 'connect'
-        return {
-          toggleIsSelected: (id: number) => {
-            dispatch({
-              type: ActionTypes.TOGGLE_IS_ACTIVE,
-              payload: {
-                id
-              }
-            })
-          }
+    key: props => `iNavNodesContainerItem_${props.index}`,
+    createStore: (props: any) => {
+      return createStore(componentRootReducer, { id: props.index, isActive: false }) // NOTE: if list dynamically changes, id may be incorrect as id is set only on inital state.
+    },
+    mapDispatchToProps: (dispatch: Dispatch<any>) => {
+      return {
+        toggleIsSelected: (id: number) => {
+          dispatch({
+            type: ActionTypes.TOGGLE_IS_ACTIVE,
+            payload: {
+              id
+            }
+          })
         }
-      },
-      filterGlobalActions: (action: any) => {
-        // Any logic to determine if the actions should be forwarded
-        // to the component's reducer. By default none is except those
-        // originated by component itself
-        const allowedActions = [ActionTypes.TOGGLE_IS_ACTIVE];
-        return allowedActions.indexOf(action.type) !== -1;
+      }
+    },
+    filterGlobalActions: (action: any) => {
+      const allowedActions = [ActionTypes.TOGGLE_IS_ACTIVE];
+      return allowedActions.indexOf(action.type) !== -1;
     }
   })
-);
+)
 
 const INavMenuHeaderItem = wrapper(INavMenuHeaderItemComponent)
-  
-export default INavMenuHeaderItem
 
-const rootReducer = (state: any, action: any): any => {
-      // state represents *only* the UI state for this component's scope - not any children
-      switch (action.type) {
-      case ActionTypes.TOGGLE_IS_ACTIVE:
-          return {
-              ...state,
-              isActive: state.id === action.payload.id
-          }
-      default:
-          return state
-      }
-  }
+export default INavMenuHeaderItem
--- Src/Csn.Retail.Editorial.Web/Features/Listings/Assets/Js/Modules/iNav/Containers/iNavNodesContainer.tsx
+++ Src/Csn.Retail.Editorial.Web/Features/Listings/Assets/Js/Modules/iNav/Containers/iNavNodesContainer.tsx
@@ -8,64 +8,35 @@ import { createStore, compose } from 'redux';
 
 //Wrapper component
 const iNavNodes = ({ nodes }) => {
-    return <INavMenuHeader nodes={nodes} />    
+  return <INavMenuHeader nodes={nodes} />
 }
 
 //Selectors
 const getiNavNodes = (iNav) => iNav.iNav.nodes
 
-const getFilteredNodes =
-    (iNavNodes) => {
-        //Check if it has sub categories
-        //TODO: add checks for iNav.iNav.nodes - right now we just expect the data to be correct
-        const nodesfiltered = iNavNodes.filter(function (node) {
-            return !!node.facets 
-        })
+const getFilteredNodes = createSelector(
+  getiNavNodes,
+  (iNavNodes) => {
+      //Check if it has sub categories
+      //TODO: add checks for iNav.iNav.nodes - right now we just expect the data to be correct
+      const nodesfiltered = iNavNodes.filter(function (node) {
+          return !!node.facets 
+      })
 
-        return nodesfiltered
-    }
+      return nodesfiltered
+  }
+)
 
 // Redux Connect
 const mapStateToProps = (state: any) => {
-    return {
-        nodes: getFilteredNodes(state.iNav.iNav.nodes)
-    }
+  return {
+    nodes: getFilteredNodes(state.iNav)
+  }
 }
 
-const wrapper = compose(
-    
-    connect(
-        mapStateToProps
-    ),
+const INavNodesContainer =   connect(
+  mapStateToProps
+)(iNavNodes)
 
-    ui({
-        key: 'iNavNodesContainer',
-        createStore: (props: any) => {
-            return createStore(rootReducer, { isVisibleId: null })
-        },
-        filterGlobalActions: (action: any) => {
-            // Any logic to determine if the actions should be forwarded
-            // to the component's reducer. By default none is except those
-            // originated by component itself
-            const allowedActions = [ActionTypes.TOGGLE_IS_ACTIVE];
-            return allowedActions.indexOf(action.type) !== -1;
-        }
-    })
-);
-
-const INavNodesContainer = wrapper(iNavNodes)
-    
 export default INavNodesContainer
 
-const rootReducer = (state: any, action: any): any => {
-        // state represents *only* the UI state for this component's scope - not any children
-        switch (action.type) {
-        case ActionTypes.TOGGLE_IS_ACTIVE:
-            return {
-                ...state,
-                isVisibleId: action.payload.id
-            }
-        default:
-            return state
-        }
-    }
