6424fe09df165da6c2c110205a1d056244f08533
james.christou <james.christou@carsales.com.au>
2017-10-02 12:11:55 +1100

    [EDITORIAL-153]: Removing webtrends tracking

--- Src/Csn.Retail.Editorial.Web/Features/Details/GetArticleResponse.cs
+++ Src/Csn.Retail.Editorial.Web/Features/Details/GetArticleResponse.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Web;
+﻿using System.Net;
 using Csn.Retail.Editorial.Web.Features.Details.Models;
 
 namespace Csn.Retail.Editorial.Web.Features.Details
