3faf4a3c2087236a8fc6b47e3d38a567cc6a1f2b
Ali Raza <ali.raza@carsales.com.au>
2017-10-06 11:10:33 +1100

    updated react, react-dom packages and typings

--- Src/Csn.Retail.Editorial.Web/Features/Shared/Assets/Js/Modules/global.d.ts
+++ Src/Csn.Retail.Editorial.Web/Features/Shared/Assets/Js/Modules/global.d.ts
@@ -1,9 +1,3 @@
-declare var require: {
-    <T>(path: string): T;
-    (paths: string[], callback: (...modules: any[]) => void): void;
-    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
-};
-
 declare module 'Endpoints/endpoints'
 declare module 'ReactReduxUI'
 declare module 'iNav/Components/iNavMenuHeader'
