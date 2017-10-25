bb63ff7cb05fab1ff5478c9acbfce865a8ab4949
philippe.roy <philippe.roy@carsales.com.au>
2017-10-25 16:27:05 +1100

    Bugfix - API refinment & aspect fix

--- Src/Csn.Retail.Editorial.Web/Features/Shared/Assets/Js/Modules/Endpoints/endpoints.js
+++ Src/Csn.Retail.Editorial.Web/Features/Shared/Assets/Js/Modules/Endpoints/endpoints.js
@@ -3,5 +3,5 @@ export const iNav = {
     api: '/editorial/api/v1/search/nav',
     aspect: '/editorial/api/v1/search/nav/aspects/',
     home: (query = '', offset = 0, sortOrder = 'Latest') => `/editorial/beta-results/?q=${query}&offset=${offset}&sortOrder=${sortOrder}`,
-    refinement: (aspect, refinementAspect, parentExpression, query='')=>`/editorial/api/v1/search/nav/aspects/${aspect}/refinements/${query}&refinementAspect=${refinementAspect}&parentExpression=${parentExpression}`
+    refinement: (aspect, refinementAspect, parentExpression, query='')=>`/editorial/api/v1/search/nav/aspects/${aspect}/refinements/${ query.length ? query : '?' }&refinementAspect=${refinementAspect}&parentExpression=${parentExpression}`
 }
