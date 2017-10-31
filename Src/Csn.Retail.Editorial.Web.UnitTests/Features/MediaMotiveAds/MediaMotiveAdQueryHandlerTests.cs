2e3e9aa8698afae7558e6ec7101a8cfd2c10abda
philippe.roy <philippe.roy@carsales.com.au>
2017-10-31 16:39:58 +1100

    Limit AdUnits depending on vertical

--- Src/Csn.Retail.Editorial.Web.UnitTests/Features/MediaMotiveAds/MediaMotiveAdQueryHandlerTests.cs
+++ Src/Csn.Retail.Editorial.Web.UnitTests/Features/MediaMotiveAds/MediaMotiveAdQueryHandlerTests.cs
@@ -1,22 +1,33 @@
 ﻿using System.Collections.Generic;
+using Csn.MultiTenant;
 using Csn.Retail.Editorial.Web.Features.MediaMotiveAds;
 using Csn.Retail.Editorial.Web.Features.MediaMotiveAds.TagBuilders;
+using Csn.Retail.Editorial.Web.Features.Shared.Models;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Csn.Retail.Editorial.Web.UnitTests.Features.MediaMotiveAds
 {
     class MediaMotiveAdQueryHandlerTests
     {
+        
         [Test]
         public void MultipleTagsWithDuplicates()
         {
+            var tenantProvider = Substitute.For<ITenantProvider<TenantInfo>>();
+            tenantProvider.Current().Returns(new TenantInfo()
+            {
+                Name = "carsales",
+                AdUnits = new List<string> { "tile3" }
+            });
             var tagBuilders = new List<IMediaMotiveTagBuilder>()
             {
                 new TestTagBuilder()
             };
 
-            var queryHandler = new MediaMotiveAdQueryHandler(tagBuilders);
+            var queryHandler = new MediaMotiveAdQueryHandler(tagBuilders, tenantProvider);
 
+            //Act
             var result = queryHandler.Handle(new MediaMotiveAdQuery()
             {
                 TileId = "Tile3"
