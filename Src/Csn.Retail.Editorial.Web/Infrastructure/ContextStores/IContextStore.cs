dca795c33b224a7a1642e3a75ffddf20fd86df5c
Ali Raza <ali.raza@carsales.com.au>
2017-11-14 13:24:59 +1100

    removed commented code in IContextStore

--- Src/Csn.Retail.Editorial.Web/Infrastructure/ContextStores/IContextStore.cs
+++ Src/Csn.Retail.Editorial.Web/Infrastructure/ContextStores/IContextStore.cs
@@ -6,57 +6,6 @@ using Csn.Retail.Editorial.Web.Infrastructure.Attributes;
 
 namespace Csn.Retail.Editorial.Web.Infrastructure.ContextStores
 {
-    //public interface IContextStore
-    //{
-    //    T GetOrFetch<T>(string key, Func<T> fetch);
-    //}
-
-    //[AutoBindAsPerRequest]
-    //public class ContextStore : IContextStore
-    //{
-    //    private readonly ConcurrentDictionary<string, object> store;
-
-    //    public ContextStore()
-    //    {
-    //        store = new ConcurrentDictionary<string, object>();
-    //    }
-
-    //    [DebuggerStepThrough]
-    //    private object Get(string name)
-    //    {
-    //        return store.TryGetValue(name, out object result) ? result : null;
-    //    }
-
-    //    [DebuggerStepThrough]
-    //    private void Set(string name, object value)
-    //    {
-    //        store.TryAdd(name, value);
-    //    }
-
-    //    private bool Exists(string key)
-    //    {
-    //        return HttpContext.Current != null && HttpContext.Current.Items.Contains(key);
-    //    }
-
-    //    public T GetOrFetch<T>(string key, Func<T> fetch)
-    //    {
-    //        T result;
-
-    //        if (Exists(key))
-    //        {
-    //            result = (T)Get(key);
-    //        }
-    //        else
-    //        {
-    //            result = fetch.Invoke();
-
-    //            Set(key, result);
-    //        }
-
-    //        return result;
-    //    }
-    //}
-
     public interface IContextStore<T>
     {
         T Get();
