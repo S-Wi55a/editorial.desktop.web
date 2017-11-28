e00886227072d7e6152ca321dec13976853d4a91
Ali Raza <ali.raza@carsales.com.au>
2017-11-28 15:03:29 +1100

    used ryvus query from results to create CSN data tags

--- Src/Csn.Retail.Editorial.Web/Features/Listings/GetListingsQueryHandler.cs
+++ Src/Csn.Retail.Editorial.Web/Features/Listings/GetListingsQueryHandler.cs
@@ -127,7 +127,7 @@ namespace Csn.Retail.Editorial.Web.Features.Listings
                     DisqusSource = _tenantProvider.Current().DisqusSource,
                     PolarNativeAdsData = _polarNativeAdsDataMapper.Map(resultData.INav.BreadCrumbs),
                     ShowSponsoredLinks = _sponsoredLinksDataMapper.ShowSponsoredLinks(),
-                    InsightsData = _listingInsightsDataMapper.Map(query.Q, query.Sort), //how to build tags with seo fragments
+                    InsightsData = _listingInsightsDataMapper.Map(resultData.Metadata?.Query, query.Sort),
                     SeoData = _seoDataMapper.Map(resultData)
                 }
             };
