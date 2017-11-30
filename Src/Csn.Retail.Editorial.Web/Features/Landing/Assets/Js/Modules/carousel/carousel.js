f761ad5162beeccd1d6b641fdd56ab8462b08800
philippe.roy <philippe.roy@carsales.com.au>
2017-11-30 12:28:05 +1100

    Add Carousel React Comp and Redux

---
+++ Src/Csn.Retail.Editorial.Web/Features/Landing/Assets/Js/Modules/carousel/carousel.js
@@ -0,0 +1,37 @@
+﻿import 'react-hot-loader/patch';
+import React from 'react'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { AppContainer } from 'react-hot-loader';
+import Carousel from 'carousel/Component/carouselComponent'
+
+//Check for Store
+const store = window.store
+
+const render = (WrappedComponent) => (container) => {
+    ReactDOM.hydrate(
+        <AppContainer carousel={container.id} >
+            <Provider store={store}>
+                <WrappedComponent />
+            </Provider>
+        </AppContainer>,
+        container
+    );
+};
+
+const component = render(Carousel)
+
+export default component
+
+//If store exists
+//if (store) {
+    
+//    //Render Carousel Components
+//    render(Carousel)
+
+//    if (module.hot) {
+//        module.hot.accept('iNav/Containers/iNavContainer', () => render(INav));
+//    }
+//}
+
+
--- Src/Csn.Retail.Editorial.Web/Features/Landing/Assets/Js/landing-page.js
+++ Src/Csn.Retail.Editorial.Web/Features/Landing/Assets/Js/landing-page.js
@@ -1,4 +1,22 @@
-﻿//require('Css/landing-page.scss');
+﻿require('Css/landing-page.scss');
 
+import { configureStore } from 'Redux/Global/Store/store.client.js'
+if (process.env.DEBUG) { require('debug.addIndicators'); }
 
+//Enable Redux store globally
+window.store = configureStore(); //Init store
 
+// load Redux 
+(function redux(d) {
+
+    // Check if there is a preloaded state
+    const initState = window.__PRELOADED_STATE__store.landing
+
+    window.store.addReducer('store', require('carousel/Reducers').carouselParentReducer(initState));
+        
+    if (d.querySelector('.csn-carousel')) {
+        const carousels = [...document.querySelectorAll('.csn-carousel')]
+        carousels.forEach((el) => require('carousel/carousel').default(el))
+    }
+
+})(document);
