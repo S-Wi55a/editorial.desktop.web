28db5c1f1227b6a4145b87dae8f0ecd723d06257
james.christou <james.christou@carsales.com.au>
2017-12-06 10:22:10 +1100

    Making react serverside setup configurable

---
+++ Src/Csn.Retail.Editorial.Web/Ioc/SettingsModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Csn.Retail.Editorial.Web.Features.Shared.Settings;
+using Csn.Retail.Editorial.Web.Infrastructure.Settings;
+
+namespace Csn.Retail.Editorial.Web.Ioc
+{
+    public class SettingsModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(x => EditorialSettings.Instance).As<EditorialSettings>().SingleInstance();
+            builder.Register(x => VideosApiSettings.Instance).As<VideosApiSettings>().SingleInstance();
+            builder.Register(x => ReactNetSettings.Instance).As<ReactNetSettings>().SingleInstance();
+        }
+    }
+}
