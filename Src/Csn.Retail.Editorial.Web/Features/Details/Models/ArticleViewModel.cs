991cb3acc171b8533bc6323d8704f05c12bcc0f3
james.christou <james.christou@carsales.com.au>
2018-02-13 14:52:25 +1100

    [EDITORIAL-2195]: Passing utm tracking tags through to brightcove iframe url

--- Src/Csn.Retail.Editorial.Web/Features/Details/Models/ArticleViewModel.cs
+++ Src/Csn.Retail.Editorial.Web/Features/Details/Models/ArticleViewModel.cs
@@ -45,10 +45,7 @@ namespace Csn.Retail.Editorial.Web.Features.Details.Models
     {
         public HeroType Type { get; set; }
         public List<Image> Images { get; set; }
-        public string BrightcovePlayerId { get; set; }
-        public string BrightcoveVideoId { get; set; }
-        public string BrightcoveVideoIFrameUrl { get; set; }
-        public string BrightcoveVideoEncodingUrl { get; set; }
+        public BrightcoveVideo BrightcoveVideo { get; set; }
         public string Headline { get; set; }
         public string SubHeading { get; set; }
         public string NetworkId { get; set; }
--- Src/Csn.Retail.Editorial.Web/Infrastructure/Redirects/HttpRequestContextWrapper.cs
+++ Src/Csn.Retail.Editorial.Web/Infrastructure/Redirects/HttpRequestContextWrapper.cs
@@ -15,12 +15,6 @@ namespace Csn.Retail.Editorial.Web.Infrastructure.Redirects
         NameValueCollection QueryString { get; }
 
         Uri Url { get; }
-
-        bool IsMobile();
-
-        bool IsIpad();
-
-        bool IsTouchDevice();
     }
 
     [AutoBindAsPerRequest]
@@ -47,66 +41,5 @@ namespace Csn.Retail.Editorial.Web.Infrastructure.Redirects
         }
 
         public Uri Url { get { return HttpContext.Current.NullSafeGet(x => x.Request.Url); } }
-
-        public bool IsMobile()
-        {
-            if (HttpContext.Current == null || HttpContext.Current.Request == null) return false;
-
-            var request = HttpContext.Current.Request;
-
-            bool isMobile = (request.Browser != null && request.Browser.IsMobileDevice);
-
-            string userAgent = (!string.IsNullOrEmpty(request.UserAgent)) ? request.UserAgent.ToLower() : string.Empty;
-
-            // Check for mobile devices, excluding iPad.
-            if ((isMobile
-                || userAgent.Contains("iphone")
-                || userAgent.Contains("ipod")
-                || userAgent.Contains("blackberry")
-                || userAgent.Contains("mobile")
-                || userAgent.Contains("windows ce")
-                || userAgent.Contains("opera mini")
-                || userAgent.Contains("palm"))
-                && !userAgent.Contains("ipad"))
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public bool IsIpad()
-        {
-            return HttpContext.Current.NullSafeGet(cxt => cxt
-                        .Request
-                        .UserAgent
-                        .NullSafe()
-                        .IndexOf("ipad", StringComparison.OrdinalIgnoreCase) >= 0);
-        }
-
-        public bool IsTouchDevice()
-        {
-            if (HttpContext.Current == null || HttpContext.Current.Request == null) return false;
-
-            var request = HttpContext.Current.Request;
-
-            bool isMobile = (request.Browser != null && request.Browser.IsMobileDevice);
-
-            string userAgent = (!string.IsNullOrEmpty(request.UserAgent)) ? request.UserAgent.ToLower() : string.Empty;
-
-            if (isMobile
-               || userAgent.Contains("iphone")
-               || userAgent.Contains("ipod")
-               || userAgent.Contains("blackberry")
-               || userAgent.Contains("mobile")
-               || userAgent.Contains("windows ce")
-               || userAgent.Contains("opera mini")
-               || userAgent.Contains("palm")
-               || userAgent.Contains("ipad")
-               || userAgent.Contains("touch"))
-            {
-                return true;
-            }
-            return false;
-        }
     }
 }
