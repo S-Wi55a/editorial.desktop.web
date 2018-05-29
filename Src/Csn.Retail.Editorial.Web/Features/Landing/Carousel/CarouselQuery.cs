bbeff11e6f8cb22a0cf6f44f411b541e0d9aedbc
jony.wijaya <jony.wijaya@carsales.com.au>
2018-05-29 16:29:30 +1000

    Introduce Offset Limit to the configuration

--- Src/Csn.Retail.Editorial.Web/Features/Landing/Carousel/CarouselQuery.cs
+++ Src/Csn.Retail.Editorial.Web/Features/Landing/Carousel/CarouselQuery.cs
@@ -6,6 +6,7 @@ namespace Csn.Retail.Editorial.Web.Features.Landing.Carousel
     {
         public string Q { get; set; }
         public int Offset { get; set; }
+        public int Limit { get; set; }
         public string Sort { get; set; }
     }
 }
