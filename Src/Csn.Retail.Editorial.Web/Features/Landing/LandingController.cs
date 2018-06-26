d6f01864ed1af4beab4d9af1751f3a7d5ea3792a
james.christou <james.christou@carsales.com.au>
2018-06-26 10:41:24 +1000

    Updating custom errors path to use our editorial error pages

--- Src/Csn.Retail.Editorial.Web/Features/Landing/LandingController.cs
+++ Src/Csn.Retail.Editorial.Web/Features/Landing/LandingController.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Web.Mvc;
 using Csn.Retail.Editorial.Web.Features.Errors;
 using Csn.Retail.Editorial.Web.Features.Listings.Filters;
