36a564c6a858ce92d42e14ab19bbbeb57b458220
Mengya <mengya.wang@carsales.com.au>
2018-07-05 11:06:34 +1000

    Add soloautos as a tenant, and set up build scripts for au and latam separately

--- Src/Csn.Retail.Editorial.Web/WebpackConfig/Shared/tenants.config.js
+++ Src/Csn.Retail.Editorial.Web/WebpackConfig/Shared/tenants.config.js
@@ -10,8 +10,36 @@ export const listofTenants = [
     'trucksales',
     'caravancampingsales',
     'farmmachinerysales',
+    'redbook',
+    'soloautos'
+];
+
+export const AUTenants = [
+    'carsales',
+    'constructionsales',
+    'bikesales',
+    'boatsales',
+    'trucksales',
+    'caravancampingsales',
+    'farmmachinerysales',
     'redbook'
 ];
 
+export const LATAMTenants = [
+    'soloautos'
+];
 
-export const TENANTS = process.env.TENANT ? [process.env.TENANT.trim().toLowerCase()] : listofTenants;
+const getTenants = (tenant) => {
+    switch (tenant) {
+        case "au":
+            return AUTenants;
+        case "latam":
+            return LATAMTenants;
+
+        default:
+            return [tenant];
+    }
+}
+
+export const TENANTS = process.env.TENANT ? getTenants(process.env.TENANT.trim().toLowerCase()) : listofTenants;
+//export const TENANTS = process.env.TENANT ? [process.env.TENANT.trim().toLowerCase()] : listofTenants;
