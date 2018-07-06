acc7ac808b5784b6afa14fc81e1ebd4e4cd20913
Ali Raza <ali.raza@carsales.com.au>
2018-07-06 15:27:11 +1000

    fixed unit test

--- Src/Csn.Retail.Editorial.Web.UnitTests/Redirections/ListingsUrlBuilderTests.cs
+++ Src/Csn.Retail.Editorial.Web.UnitTests/Redirections/ListingsUrlBuilderTests.cs
@@ -9,9 +9,9 @@ namespace Csn.Retail.Editorial.Web.UnitTests.Redirections
 {
     public class ListingsUrlBuilderTests
     {
-        [TestCase(true, "/query/",  "abc", 30, "latest", "/editorial/results/query/?sb=latest&pg=30")]
+        [TestCase(true, "/query/",  "abc", 30, "latest", "/editorial/query/?sb=latest&pg=30")]
         [TestCase(true, "/query/", "abc", 0, "", "/editorial/query/")]
-        [TestCase(true, "/query/", "abc", 30, "", "/editorial/results/query/?pg=30")]
+        [TestCase(true, "/query/", "abc", 30, "", "/editorial/query/?pg=30")]
         [TestCase(false, "query", "abc", 30, "latest", "/editorial/results/?q=query&sb=latest&keywords=abc&pg=30")]
         [TestCase(false, "query", "abc", 0, "", "/editorial/results/?q=query&keywords=abc")]
         [TestCase(false, "query", "abc", 30, "", "/editorial/results/?q=query&keywords=abc&pg=30")]
