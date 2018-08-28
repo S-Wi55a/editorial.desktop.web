c7090b523e6d553ee29d613cd920e7baacc1b039
Mengya Wang <mengya.wang@carsales.office>
2018-08-28 15:14:10 +1000

    Change ServiceName to TenantName

--- Src/Csn.Retail.Editorial.Web/Features/Details/GetArticleQueryHandler.cs
+++ Src/Csn.Retail.Editorial.Web/Features/Details/GetArticleQueryHandler.cs
@@ -28,7 +28,7 @@ namespace Csn.Retail.Editorial.Web.Features.Details
         {
             var result = await _editorialApiProxy.GetArticleAsync(new EditorialApiInput()
             {
-                ServiceName = _tenantProvider.Current().RyvusServiceProjection,
+                ServiceName = _tenantProvider.Current().TenantName,
                 ViewType = "desktop",
                 Id = query.Id,
                 IsPreview = query.IsPreview
