89f00c7b7374e76f801c0972e0e36fcb10772d1f
james.christou <james.christou@carsales.com.au>
2018-09-26 09:09:24 +1000

    Fixing issue with tracking api not supporting IncludeNielsen parameter for latam

--- Src/Csn.Retail.Editorial.Web/Features/Shared/Proxies/TrackingApi/TrackingApiProxy.cs
+++ Src/Csn.Retail.Editorial.Web/Features/Shared/Proxies/TrackingApi/TrackingApiProxy.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using Csn.MultiTenant;
+using Csn.Retail.Editorial.Web.Features.Shared.Models;
 using Csn.Retail.Editorial.Web.Infrastructure.Attributes;
 using Ingress.ServiceClient.Abstracts;
 
@@ -14,19 +16,26 @@ namespace Csn.Retail.Editorial.Web.Features.Shared.Proxies.TrackingApi
     {
         private const string ServiceName = "api-tracking-ga-scripts";
         private readonly ISmartServiceClient _smartClient;
+        private readonly ITenantProvider<TenantInfo> _tenantProvider;
 
-        public TrackingApiProxy(ISmartServiceClient smartClient)
+        public TrackingApiProxy(ISmartServiceClient smartClient, ITenantProvider<TenantInfo> tenantProvider)
         {
             _smartClient = smartClient;
+            _tenantProvider = tenantProvider;
         }
 
         public Task<SmartServiceResponse<TrackingApiDto>> GetTracking(TrackingApiInput input)
         {
-            return _smartClient.Service(ServiceName)
+            var client = _smartClient.Service(ServiceName)
                 .Path("v1/api/tracking/script")
-                .QueryString("Application", input.ApplicationName)
-                .QueryString("IncludeNielsen", "true")
-                .GetAsync<TrackingApiDto>();
+                .QueryString("Application", input.ApplicationName);
+
+            if (_tenantProvider.Current().IncludeNielsen)
+            {
+                client.QueryString("IncludeNielsen", "true");
+            }
+                
+            return client.GetAsync<TrackingApiDto>();
         }
     }
 }
