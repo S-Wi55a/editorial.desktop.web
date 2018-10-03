085bc694cb9642373e8e208d0a7437686a498bd8
Ali Raza <ali.raza@carsales.com.au>
2018-10-04 09:21:13 +1000

    Initial commit with globalization & language resources

---
+++ Src/Csn.Retail.Editorial.Web/Culture/LanguageConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Csn.Retail.Editorial.Web.Culture
+{
+    public static class LanguageConstants
+    {
+        public static string ViewAll => "ViewAll";
+    }
+}
