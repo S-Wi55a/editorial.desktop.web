e87f29f31cd622a02bd089991b90c32367e81e52
jony.wijaya <jony.wijaya@carsales.com.au>
2018-10-26 13:14:32 +1100

    Width & Height are int

--- Src/Csn.Retail.Editorial.Web/Infrastructure/Extensions/ImageExtensions.cs
+++ Src/Csn.Retail.Editorial.Web/Infrastructure/Extensions/ImageExtensions.cs
@@ -4,9 +4,9 @@ namespace Csn.Retail.Editorial.Web.Infrastructure.Extensions
 {
     public static class ImageExtensions
     {
-        public static string GetHeroImageUrl(this Image image, string width, string height)
+        public static string GetHeroImageUrl(this Image image, int width=0, int height=0)
         {
-            var imageUrl = $"{image.Url}?width={width}&height={height}" + (image.NeedsCropping ? "&pxc_method=crop" : "");
+            var imageUrl = $"{image.Url}?width={width}" + (height > 0 ? "&height=" + height : "") + (image.NeedsCropping ? "&pxc_method=crop" : "");
 
             return imageUrl;
         }
