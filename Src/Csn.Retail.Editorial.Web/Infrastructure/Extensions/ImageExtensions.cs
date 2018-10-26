354108abfe7f9add82681af5ccdb469463915844
jony.wijaya <jony.wijaya@carsales.com.au>
2018-10-26 11:48:42 +1100

    Added ImageExtensions to build the image URL.

---
+++ Src/Csn.Retail.Editorial.Web/Infrastructure/Extensions/ImageExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Csn.Retail.Editorial.Web.Features.Shared.Proxies.EditorialApi;
+
+namespace Csn.Retail.Editorial.Web.Infrastructure.Extensions
+{
+    public static class ImageExtensions
+    {
+        public static string GetHeroImageUrl(this Image image, string width, string height)
+        {
+            var imageUrl = $"{image.Url}?width={width}&height={height}" + (image.NeedsCropping ? "&pxc_method=crop" : "");
+
+            return imageUrl;
+        }
+    }
+}
