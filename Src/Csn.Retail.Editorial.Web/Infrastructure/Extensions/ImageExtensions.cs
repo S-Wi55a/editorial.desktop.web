7c34d4a5626fdc16278360628795827dcadaa854
jony.wijaya <jony.wijaya@carsales.com.au>
2018-10-26 15:25:25 +1100

    Refactored imageUrl.

--- Src/Csn.Retail.Editorial.Web/Infrastructure/Extensions/ImageExtensions.cs
+++ Src/Csn.Retail.Editorial.Web/Infrastructure/Extensions/ImageExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Csn.Retail.Editorial.Web.Features.Shared.Proxies.EditorialApi;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Csn.Retail.Editorial.Web.Features.Shared.Proxies.EditorialApi;
 
 namespace Csn.Retail.Editorial.Web.Infrastructure.Extensions
 {
@@ -6,7 +9,18 @@ namespace Csn.Retail.Editorial.Web.Infrastructure.Extensions
     {
         public static string GetHeroImageUrl(this Image image, int width=0, int height=0)
         {
-            var imageUrl = $"{image.Url}?width={width}" + (height > 0 ? "&height=" + height : "") + (image.NeedsCropping ? "&pxc_method=crop" : "");
+            var queryString = new Dictionary<string, string>();
+
+            if (width > 0)
+                queryString.Add("width", width.ToString());
+
+            if (height > 0)
+                queryString.Add("height", height.ToString());
+
+            if (image.NeedsCropping)
+                queryString.Add("pxc_method", "crop");
+
+            var imageUrl = string.Format(image.Url + "?{0}", string.Join("&", queryString.Select(kvp => $"{kvp.Key}={kvp.Value}")));
 
             return imageUrl;
         }
