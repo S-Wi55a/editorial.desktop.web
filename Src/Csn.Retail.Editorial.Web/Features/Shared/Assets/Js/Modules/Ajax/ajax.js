014bc0a44092e890fbe5de26df5058d37013eb73
Mengya Wang <mengya.wang@carsales.office>
2019-01-11 17:14:56 +1100

    Clean up

--- Src/Csn.Retail.Editorial.Web/Features/Shared/Assets/Js/Modules/Ajax/ajax.js
+++ Src/Csn.Retail.Editorial.Web/Features/Shared/Assets/Js/Modules/Ajax/ajax.js
@@ -1,4 +1,4 @@
-﻿function get(url, success = (resp) => { }, error = ()=>{}) {
+﻿function get(url, success = (resp) => { }, error = ()=>{}){
 
         if (url === null || !url.length) {throw new Error('Invalid URL')}
 
@@ -34,28 +34,7 @@
         //    });
 }
 
-function promisedGet(url) {
-    return new Promise((resolve, reject) => {
-        let request = new XMLHttpRequest();
-        request.open("GET", url);
-        
-        request.onload = () => {
-            if (request.status >= 200 && request.status < 400) {
-                // Success!
-                resolve(request.responseText);
-            } else {
-                // Error
-                reject(request.statusText);
-            }
-        };
-
-        request.onerror = () => reject(request.statusText);
-        request.send();
-    });
-}
-
-
-export { get, promisedGet }
+export { get }
 
 
 
