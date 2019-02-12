d1a62cc97de4e61c0589f7b5b59f5c5f48a7e070
Mengya <mengya.wang@carsales.com.au>
2019-02-12 18:38:18 +1100

    Do not push new state to history to avoid navigate back inside modal

--- Src/Csn.Retail.Editorial.Web/Features/Details/Assets/details-page.js
+++ Src/Csn.Retail.Editorial.Web/Features/Details/Assets/details-page.js
@@ -183,7 +183,8 @@ if(!document.querySelector('body').classList.contains('ie') && !isMobile.tablet 
     const aside = document.querySelector('.aside');
     loaded.then(function() {     
         if (aside) {
-            require('Js/Modules/StickySidebar/stickySidebar.js').init(document, window, aside, 'article .article', 137);
+            const topReference = !csn_editorial.detailsModal ? 0 : -20;
+            require('Js/Modules/StickySidebar/stickySidebar.js').init(document, window, aside, 'article .article', 137, topReference);
         }
     })
 }
