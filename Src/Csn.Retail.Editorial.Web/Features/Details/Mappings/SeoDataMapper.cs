5326dd11cc1e8242e6fc8456f89d01107a0cf940
Ali Raza <ali.raza@carsales.com.au>
2019-02-18 14:20:05 +1100

    removed configurable noindex, nofollow meta tags from solution

--- Src/Csn.Retail.Editorial.Web/Features/Details/Mappings/SeoDataMapper.cs
+++ Src/Csn.Retail.Editorial.Web/Features/Details/Mappings/SeoDataMapper.cs
@@ -1,43 +1,25 @@
-﻿using Csn.MultiTenant;
-using Csn.Retail.Editorial.Web.Features.Shared.Models;
-using Csn.Retail.Editorial.Web.Infrastructure.Attributes;
+﻿using Csn.Retail.Editorial.Web.Features.Shared.Models;
 
 namespace Csn.Retail.Editorial.Web.Features.Details.Mappings
 {
     public interface ISeoDataMapper
     {
-        SeoData Map(Shared.Proxies.EditorialApi.SeoData article);
+        SeoData Map(Shared.Proxies.EditorialApi.SeoData seoData);
     }
 
-    [AutoBind]
-    public class SeoDataMapper : ISeoDataMapper
+    public class SeoDataMapper: ISeoDataMapper
     {
-        private readonly ITenantProvider<TenantInfo> _tenantInfo;
-
-        public SeoDataMapper(ITenantProvider<TenantInfo> tenantInfo)
-        {
-            _tenantInfo = tenantInfo;
-        }
-
         public SeoData Map(Shared.Proxies.EditorialApi.SeoData seoData)
         {
-            if (seoData == null)
-            {
-                return new SeoData()
-                {
-                    AllowSeoIndexing = _tenantInfo.Current().AllowSeoIndexOfDetails
-                };
-            }
-
-            return new SeoData()
+            if (seoData == null) return null;
+            return new SeoData
             {
                 Title = seoData.Title,
                 AlternateUrl = seoData.AlternateUrl,
                 CanonicalUrl = seoData.CanonicalUrl,
                 CanonicalAmpUrl = seoData.CanonicalAmpUrl,
                 Description = seoData.Description,
-                Keywords = seoData.Keywords,
-                AllowSeoIndexing = _tenantInfo.Current().AllowSeoIndexOfDetails
+                Keywords = seoData.Keywords
             };
         }
     }
