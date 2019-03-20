f25ba3c8e8de2753cfc9a60548b9b9a9fa65f259
Ali Raza <ali.raza@carsales.com.au>
2019-03-20 15:08:24 +1100

    chileautos adds added - AdSettings still need updates

--- Src/Csn.Retail.Editorial.Web/Features/DisplayAds/GoogleAd/GoogleAdSettings.cs
+++ Src/Csn.Retail.Editorial.Web/Features/DisplayAds/GoogleAd/GoogleAdSettings.cs
@@ -4,56 +4,115 @@ namespace Csn.Retail.Editorial.Web.Features.DisplayAds.GoogleAd
 {
     public class GoogleAdSettings
     {
-        public static Dictionary<DisplayAdPlacements, GoogleAdSetting> GoogleAdTypes = new Dictionary<DisplayAdPlacements, GoogleAdSetting>
-        {
+        public static Dictionary<string, Dictionary<DisplayAdPlacements, GoogleAdSetting>> GoogleAdTypes =
+            new Dictionary<string, Dictionary<DisplayAdPlacements, GoogleAdSetting>>
             {
-                DisplayAdPlacements.ListingsAside,
-                new GoogleAdSetting
                 {
-                    SlotId = "div-gpt-ad-1548199795543-0",
-                    UnitId = "SA_editorial_300x250_300x600_E3",
-                    DisplayAdSize = DisplayAdSizes.MediumOrLargeRectangle
-                }
-            },
-            {
-                DisplayAdPlacements.DetailsAside,
-                new GoogleAdSetting
-                {
-                    SlotId = "div-gpt-ad-1548093016580-0'",
-                    UnitId = "SA_editorial_300x250_300x600_E2",
-                    DisplayAdSize = DisplayAdSizes.MediumOrLargeRectangle
-                }
-            },
-            {
-                DisplayAdPlacements.Banner,
-                new GoogleAdSetting
-                {
-                    // TODO:  Use leaderboard slot ad unit for now
-                    SlotId = "div-gpt-ad-1468849624568-5",
-                    UnitId = "SA_Homepage_728x90_M3_Top",
-                    DisplayAdSize = DisplayAdSizes.Block1200X100
-                }
-            },
-            {
-                DisplayAdPlacements.Leaderboard,
-                new GoogleAdSetting
-                {
-                    SlotId = "div-gpt-ad-1496703639902-0",
-                    UnitId = "SA_editorial_728x90_E1",
-                    DisplayAdSize = DisplayAdSizes.Leaderboard
-                }
-            },
-            {
-                DisplayAdPlacements.Carousel,
-                new GoogleAdSetting
-                {
-                    SlotId = "div-gpt-ad-1496704783581-0",
-                    UnitId = "SA_editorial_300x250_E2",
-                    DisplayAdSize = DisplayAdSizes.MediumRectangle
-                }
+                    "soloautos",
+                    new Dictionary<DisplayAdPlacements, GoogleAdSetting>
+                    {
+                        {
+                            DisplayAdPlacements.ListingsAside,
+                            new GoogleAdSetting
+                            {
+                                SlotId = "div-gpt-ad-1548199795543-0",
+                                UnitId = "SA_editorial_300x250_300x600_E3",
+                                DisplayAdSize = DisplayAdSizes.MediumOrLargeRectangle
+                            }
+                        },
+                        {
+                            DisplayAdPlacements.DetailsAside,
+                            new GoogleAdSetting
+                            {
+                                SlotId = "div-gpt-ad-1548093016580-0'",
+                                UnitId = "SA_editorial_300x250_300x600_E2",
+                                DisplayAdSize = DisplayAdSizes.MediumOrLargeRectangle
+                            }
+                        },
+                        {
+                            DisplayAdPlacements.Banner,
+                            new GoogleAdSetting
+                            {
+                                // TODO:  Use leaderboard slot ad unit for now
+                                SlotId = "div-gpt-ad-1468849624568-5",
+                                UnitId = "SA_Homepage_728x90_M3_Top",
+                                DisplayAdSize = DisplayAdSizes.Block1200X100
+                            }
+                        },
+                        {
+                            DisplayAdPlacements.Leaderboard,
+                            new GoogleAdSetting
+                            {
+                                SlotId = "div-gpt-ad-1496703639902-0",
+                                UnitId = "SA_editorial_728x90_E1",
+                                DisplayAdSize = DisplayAdSizes.Leaderboard
+                            }
+                        },
+                        {
+                            DisplayAdPlacements.Carousel,
+                            new GoogleAdSetting
+                            {
+                                SlotId = "div-gpt-ad-1496704783581-0",
+                                UnitId = "SA_editorial_300x250_E2",
+                                DisplayAdSize = DisplayAdSizes.MediumRectangle
+                            }
 
-            }
-        };
+                        }
+                    }
+                },
+                {
+                    "chileautos",
+                    new Dictionary<DisplayAdPlacements, GoogleAdSetting>
+                    {
+                        {
+                            DisplayAdPlacements.ListingsAside,
+                            new GoogleAdSetting
+                            {
+                                SlotId = "div-gpt-ad-1548199795543-0",
+                                UnitId = "SA_editorial_300x250_300x600_E3",
+                                DisplayAdSize = DisplayAdSizes.MediumOrLargeRectangle
+                            }
+                        },
+                        {
+                            DisplayAdPlacements.DetailsAside,
+                            new GoogleAdSetting
+                            {
+                                SlotId = "div-gpt-ad-1548093016580-0'",
+                                UnitId = "SA_editorial_300x250_300x600_E2",
+                                DisplayAdSize = DisplayAdSizes.MediumOrLargeRectangle
+                            }
+                        },
+                        {
+                            DisplayAdPlacements.Banner,
+                            new GoogleAdSetting
+                            {
+                                // TODO:  Use leaderboard slot ad unit for now
+                                SlotId = "div-gpt-ad-1468849624568-5",
+                                UnitId = "SA_Homepage_728x90_M3_Top",
+                                DisplayAdSize = DisplayAdSizes.Block1200X100
+                            }
+                        },
+                        {
+                            DisplayAdPlacements.Leaderboard,
+                            new GoogleAdSetting
+                            {
+                                SlotId = "div-gpt-ad-1496703639902-0",
+                                UnitId = "SA_editorial_728x90_E1",
+                                DisplayAdSize = DisplayAdSizes.Leaderboard
+                            }
+                        },
+                        {
+                            DisplayAdPlacements.Carousel,
+                            new GoogleAdSetting
+                            {
+                                SlotId = "div-gpt-ad-1496704783581-0",
+                                UnitId = "SA_editorial_300x250_E2",
+                                DisplayAdSize = DisplayAdSizes.MediumRectangle
+                            }
 
+                        }
+                    }
+                }
+            };
     }
 }
