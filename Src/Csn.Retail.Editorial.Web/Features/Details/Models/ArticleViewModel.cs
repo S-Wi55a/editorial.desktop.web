6c668c5a10fd7985f528207cec8fbbf78ccf72d8
James Christou <james.christou@carsales.office>
2019-03-21 13:53:08 +1100

    Changing SeoSchemaData property on ArticleViewModel to object so deserialization from JSON cache can work

--- Src/Csn.Retail.Editorial.Web/Features/Details/Models/ArticleViewModel.cs
+++ Src/Csn.Retail.Editorial.Web/Features/Details/Models/ArticleViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using Csn.Retail.Editorial.Web.Features.Shared.Models;
 using Csn.Retail.Editorial.Web.Features.Shared.Proxies.EditorialApi;
-using Csn.Retail.Editorial.Web.Features.Shared.SeoSchema.Models;
 
 namespace Csn.Retail.Editorial.Web.Features.Details.Models
 {
@@ -36,7 +35,7 @@ namespace Csn.Retail.Editorial.Web.Features.Details.Models
         public DisqusData DisqusData { get; set; }
         public MoreArticleData MoreArticleData { get; set; }
         public Shared.Models.SeoData SeoData { get; set; }
-        public ISeoSchema SeoSchemaData { get; set; }
+        public object SeoSchemaData { get; set; }
         public CsnInsightsData InsightsData { get; set; }
         public StockListingData StockListingData { get; set; }
         public AlsoConsiderData AlsoConsiderData { get; set; }
