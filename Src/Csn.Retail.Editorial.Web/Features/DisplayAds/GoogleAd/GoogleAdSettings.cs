b58c9c079f30d85fee97f1b70a7c4519ef5d55b9
jameschristou <james.christou@gmail.com>
2019-08-12 11:15:57 +1000

    Fixing demotores

--- Src/Csn.Retail.Editorial.Web/Features/DisplayAds/GoogleAd/GoogleAdSettings.cs
+++ Src/Csn.Retail.Editorial.Web/Features/DisplayAds/GoogleAd/GoogleAdSettings.cs
@@ -112,6 +112,59 @@ namespace Csn.Retail.Editorial.Web.Features.DisplayAds.GoogleAd
 
                         }
                     }
+                },
+                {
+                    "demotores",
+                    new Dictionary<DisplayAdPlacements, GoogleAdSetting>
+                    {
+                        {
+                            DisplayAdPlacements.ListingsAside,
+                            new GoogleAdSetting
+                            {
+                                SlotId = "div-gpt-ad-1553688924791-0",
+                                UnitId = "Editorial_Listings_Aside(300x250_300x600)",
+                                DisplayAdSize = DisplayAdSizes.MediumOrLargeRectangle
+                            }
+                        },
+                        {
+                            DisplayAdPlacements.DetailsAside,
+                            new GoogleAdSetting
+                            {
+                                SlotId = "div-gpt-ad-1553689007338-0",
+                                UnitId = "Editorial_Details_Aside(300x250_300x600)",
+                                DisplayAdSize = DisplayAdSizes.MediumOrLargeRectangle
+                            }
+                        },
+                        {
+                            DisplayAdPlacements.Banner,
+                            new GoogleAdSetting
+                            {
+                                // TODO:  Use leaderboard slot ad unit for now
+                                SlotId = "div-gpt-ad-1553524316965-0",
+                                UnitId = "Editorial_HomePage_728x90",
+                                DisplayAdSize = DisplayAdSizes.Block1200X100
+                            }
+                        },
+                        {
+                            DisplayAdPlacements.Leaderboard,
+                            new GoogleAdSetting
+                            {
+                                SlotId = "div-gpt-ad-1553524424201-0",
+                                UnitId = "DetailsPage_728x90",
+                                DisplayAdSize = DisplayAdSizes.Leaderboard
+                            }
+                        },
+                        {
+                            DisplayAdPlacements.Carousel,
+                            new GoogleAdSetting
+                            {
+                                SlotId = "div-gpt-ad-1553524510647-0",
+                                UnitId = "Editorial_HomePage_300x250",
+                                DisplayAdSize = DisplayAdSizes.MediumRectangle
+                            }
+
+                        }
+                    }
                 }
             };
     }
