c7d5a048650619797ed474395987df9097173837
jameschristou <james.christou@gmail.com>
2019-09-13 12:10:36 +1000

    Logging requests to EditorialApi

--- Src/Csn.Retail.Editorial.Web/Features/Shared/Proxies/EditorialApi/EditorialApiProxy.cs
+++ Src/Csn.Retail.Editorial.Web/Features/Shared/Proxies/EditorialApi/EditorialApiProxy.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
 using Bolt.Common.Extensions;
+using Csn.Logging;
 using Csn.Retail.Editorial.Web.Infrastructure.Attributes;
+using Csn.Retail.Editorial.Web.Infrastructure.Redirects;
 using Ingress.ServiceClient.Abstracts;
 
 namespace Csn.Retail.Editorial.Web.Features.Shared.Proxies.EditorialApi
@@ -17,14 +19,20 @@ namespace Csn.Retail.Editorial.Web.Features.Shared.Proxies.EditorialApi
     {
         private const string ServiceName = "api-retail-editorial";
         private readonly ISmartServiceClient _smartClient;
+        private readonly ILogger _logger;
+        private readonly IRequestContextWrapper _requestContext;
 
-        public EditorialApiProxy(ISmartServiceClient smartClient)
+        public EditorialApiProxy(ISmartServiceClient smartClient, ILogger logger, IRequestContextWrapper requestContext)
         {
             _smartClient = smartClient;
+            _logger = logger;
+            _requestContext = requestContext;
         }
 
         public Task<SmartServiceResponse<ArticleDetailsDto>> GetArticleAsync(EditorialApiInput input)
         {
+            _logger.Trace($"GetArticleAsync(): {_requestContext.Url.ToString()}");
+
             return _smartClient.Service(ServiceName)
                         .Path("v1/details/{0}/desktop/{1}/?isPreview={2}".FormatWith(input.ServiceName, input.Id, input.IsPreview))
                         .GetAsync<ArticleDetailsDto>();
@@ -32,6 +40,8 @@ namespace Csn.Retail.Editorial.Web.Features.Shared.Proxies.EditorialApi
 
         public Task<SmartServiceResponse<ArticleDetailsDto>> GetPreviewAsync(EditorialApiInput input)
         {
+            _logger.Trace($"GetPreviewAsync(): {_requestContext.Url.ToString()}");
+
             return _smartClient.Service(ServiceName)
                 .Path("v1/preview/{0}/desktop/{1}".FormatWith(input.ServiceName, input.Id))
                 .GetAsync<ArticleDetailsDto>();
@@ -39,6 +49,8 @@ namespace Csn.Retail.Editorial.Web.Features.Shared.Proxies.EditorialApi
 
         public Task<SmartServiceResponse<object>> GetAsync(string uri)
         {
+            _logger.Trace($"GetAsync(): {_requestContext.Url.ToString()}");
+
             return _smartClient.Service(ServiceName)
                 .Path(uri)
                 .GetAsync<object>();
